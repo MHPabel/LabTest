389d925bbe784e9a087d65bddccb260639c97404
Student <iammahadi1995@gmail.com>
2018-04-23 13:00:06 +0600

    view student info

---
+++ src/main/java/data/model/Student.java
@@ -0,0 +1,43 @@
+package data.model;
+
+public class Student {
+    private Integer studentId1=1;
+    private Integer studentId2=2;
+    private String studentName1=" A";
+    private String studentName2=" B";
+
+    public Student() {
+    }
+
+    public Integer getStudentId1() {
+        return studentId1;
+    }
+
+    public void setStudentId1(Integer studentId1) {
+        this.studentId1 = studentId1;
+    }
+
+    public Integer getStudentId2() {
+        return studentId2;
+    }
+
+    public void setStudentId2(Integer studentId2) {
+        this.studentId2 = studentId2;
+    }
+
+    public String getStudentName1() {
+        return studentName1;
+    }
+
+    public void setStudentName1(String studentName1) {
+        this.studentName1 = studentName1;
+    }
+
+    public String getStudentName2() {
+        return studentName2;
+    }
+
+    public void setStudentName2(String studentName2) {
+        this.studentName2 = studentName2;
+    }
+}
